02f41451295b3134df1e12c14d999864b9e34b47
sssoyeon25 <soyeon0205@duksung.ac.kr>
2025-11-23 01:25:31 +0900

    script.js 끝

---
+++ connect/static/script.js
@@ -0,0 +1,26 @@
+// script.js 슬라이드
+document.addEventListener("DOMContentLoaded", function () {
+  const container = document.querySelector(".slider-container");
+  const slides = document.querySelectorAll(".slide");
+  const slideCount = slides.length;
+
+  let currentIndex = 0;
+  const slideWidth = 100; // % 단위
+  const intervalTime = 3000; // 3초
+
+  function moveToNextSlide() {
+    currentIndex++;
+
+    if (currentIndex >= slideCount) {
+      currentIndex = 0;
+    }
+
+    container.style.transform = `translateX(-${currentIndex * slideWidth}%)`;
+  }
+
+  setInterval(moveToNextSlide, intervalTime);
+  // 초기 슬라이더 상태 설정 (첫 번째 슬라이드 활성화)
+    updateSlider();
+});
+
+
